2eb11fbd9e5519046d51a6e7fc627034646924bd
bogdan <bohdan.shevchenko1@nure.ua>
2019-05-20 07:12:18 +0300

    [NFC] Trying to read

--- nfc-reader/src/index.ts
+++ nfc-reader/src/index.ts
@@ -91,22 +91,26 @@ function startListening() {
     console.info('Getting tags');
     const tags = await device.listTags();
     console.info('Tags: ', tags);
+
     for (const tag of tags) {
       if (tag.read) {
         console.info('Readable', tag);
-        await new Promise(async (resolve, reject) => {
-          console.info('Opening tag', tag);
-          await tag.open();
-          const buffers = [];
-          for (let i = 0; i < 64; i += 1) {
-            console.info('Reading at', tag, i);
-            buffers.push(await tag.read(i));
-          }
-          console.info('closing', tag);
-          await tag.close();
-          return Buffer.concat(buffers);
-        }).then(data => emitter.emit('data', data))
-          .catch(err => emitter.emit('error', err));
+        console.info('Opening tag', tag);
+        await tag.open();
+        const buffers = [];
+        for (let i = 0; i < 64; i += 1) {
+          console.info('Reading at', tag, i);
+          buffers.push(await tag.read(i));
+        }
+        console.info('closing', tag);
+        await tag.close();
+        const data = Buffer.concat(buffers);
+        emitter.emit('data', data);
+        // await new Promise(async (resolve, reject) => {
+        //   await tag.open();
+        //   return Buffer.concat(buffers);
+        // }).then(data => emitter.emit('data', data))
+        //   .catch(err => emitter.emit('error', err));
       }
     }
   };
