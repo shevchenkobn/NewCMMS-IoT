4753097d16c10bcc206e12c6e9322e4c7551690a
bogdan <bohdan.shevchenko1@nure.ua>
2019-05-20 11:14:34 +0300

    [NFC] Trying to read using nfc module

---
+++ nfc-reader/@types/nfc-pcsc.d.ts
@@ -0,0 +1,3 @@
+declare module 'nfc-pcsc' {
+
+}
--- nfc-reader/src/index.ts
+++ nfc-reader/src/index.ts
@@ -1,15 +1,15 @@
 
-// import { nfc } from 'nfc';
+import { nfc } from 'nfc';
 // import { NFCReader } from 'libnfc-js';
-import * as Freefare from 'freefare';
+// import * as Freefare from 'freefare';
 import { Nullable } from './@types';
 import { EventEmitter } from 'events';
 import * as config from 'config';
 
 // let nfcReader: Nullable<NFCReader> = null;
-// let n: Nullable<nfc.NFC>;
-let device: Nullable<any>;
-let listener: Nullable<Promise<any>>;
+let n: Nullable<nfc.NFC>;
+// let device: Nullable<any>;
+// let listener: Nullable<Promise<any>>;
 let emitter: EventEmitter;
 
 export async function setEventEmitter(
@@ -27,45 +27,45 @@ export async function initialize(eventEmitter: EventEmitter): Promise<void> {
   }
   emitter = eventEmitter;
   // if (nfcReader) {
-  // if (n) {
-  if (device) {
+  if (n) {
+  // if (device) {
     emitter.emit('warning', 'The module is already initialized. Initialization skipped.');
     return;
   }
   // nfcReader = new NFCReader();
   // nfcReader.open();
-  // n = new nfc.NFC();
-  device = (await new Freefare().listDevices())[0];
-  console.info('Found a device:', device);
-  await device.open();
-  console.info('opened it');
+  n = new nfc.NFC();
+  // device = (await new Freefare().listDevices())[0];
+  // console.info('Found a device:', device);
+  // await device.open();
+  // console.info('opened it');
   startListening();
 }
 
 export async function dispose(): Promise<void> {
   // if (!nfcReader) {
-  // if (!n) {
-  if (!device) {
+  if (!n) {
+  // if (!device) {
     emitter.emit('warning', 'The module is not initialized.');
     return;
   }
   // nfcReader._nfc.close();
   // nfcReader = null;
-  // n.stop();
-  // n = null;
-  device.close();
-  // clearInterval(interval);
-  device = null;
-  if (listener) {
-    listener.catch(err => {});
-  }
-  listener = null;
+  n.stop();
+  n = null;
+  // device.close();
+  // // clearInterval(interval);
+  // device = null;
+  // if (listener) {
+  //   listener.catch(err => {});
+  // }
+  // listener = null;
 }
 
 function startListening() {
   // if (!nfcReader) {
-  // if (!n) {
-  if (!device) {
+  if (!n) {
+  // if (!device) {
     throw new TypeError('The module is not iniitalized');
   }
   // nfcReader.on('card', (card: any) => {
@@ -80,43 +80,43 @@ function startListening() {
   //   // });
   // });
   // nfcReader.poll();
-  // n.on('read', (tag: any) => {
-  //   console.info(tag);
-  //   emitter.emit('data', tag.data);
-  // }).on('error', (err: any) => {
-  //   emitter.emit(err);
-  // }).start();
-  listener = null;
-  const listen = async () => {
-    try {
-      console.info('Getting tags');
-      const tags = await device.listTags();
-      console.info('Tags: ', tags);
-      for (const tag of tags) {
-        if (tag.read) {
-          console.info('Readable', tag);
-          console.info('Opening tag', tag);
-          await tag.open();
-          const buffers = [];
-          for (let i = 0; i < 64; i += 1) {
-            console.info('Reading at', tag, i);
-            buffers.push(await tag.read(i));
-          }
-          console.info('closing', tag);
-          await tag.close();
-          const data = Buffer.concat(buffers);
-          emitter.emit('data', data);
-          // await new Promise(async (resolve, reject) => {
-          //   await tag.open();
-          //   return Buffer.concat(buffers);
-          // }).then(data => emitter.emit('data', data))
-          //   .catch(err => emitter.emit('error', err));
-        }
-      }
-    } catch (err) {
-      emitter.emit('error', err);
-    }
-  };
-  const wrapper = (): any => listen().catch(err => emitter.emit('error', err)).then(wrapper);
-  wrapper();
+  n.on('read', (tag: any) => {
+    console.info(tag);
+    emitter.emit('data', tag.data);
+  }).on('error', (err: any) => {
+    emitter.emit(err);
+  }).start();
+  // listener = null;
+  // const listen = async () => {
+  //   try {
+  //     console.info('Getting tags');
+  //     const tags = await device.listTags();
+  //     console.info('Tags: ', tags);
+  //     for (const tag of tags) {
+  //       if (tag.read) {
+  //         console.info('Readable', tag);
+  //         console.info('Opening tag', tag);
+  //         await tag.open();
+  //         const buffers = [];
+  //         for (let i = 0; i < 64; i += 1) {
+  //           console.info('Reading at', tag, i);
+  //           buffers.push(await tag.read(i));
+  //         }
+  //         console.info('closing', tag);
+  //         await tag.close();
+  //         const data = Buffer.concat(buffers);
+  //         emitter.emit('data', data);
+  //         // await new Promise(async (resolve, reject) => {
+  //         //   await tag.open();
+  //         //   return Buffer.concat(buffers);
+  //         // }).then(data => emitter.emit('data', data))
+  //         //   .catch(err => emitter.emit('error', err));
+  //       }
+  //     }
+  //   } catch (err) {
+  //     emitter.emit('error', err);
+  //   }
+  // };
+  // const wrapper = (): any => listen().catch(err => emitter.emit('error', err)).then(wrapper);
+  // wrapper();
 }
